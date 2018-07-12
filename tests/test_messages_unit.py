6c563c6274e49ef8ae75b215a6766dabd82b3dad
Daniel <djustice@wificidr.net>
2018-07-12 16:21:20 -0500

    Unit tests

---
+++ tests/test_messages_unit.py
@@ -0,0 +1,36 @@
+"""Message app test runner."""
+import os
+import base64
+from functools import wraps
+from pathlib import Path
+import pytest
+import json
+from unittest.mock import Mock, patch, MagicMock
+from flask import g
+from faker import Faker
+from src.app import create_app
+from src.message_bp import get_messages
+import src.models as m
+from tests.load_db_data import init_db
+
+FAKER = Faker()
+
+@pytest.fixture
+def app_inst():
+    """Pytest fixture that returns an instance of our application."""
+    app = create_app()
+    app.debug = True
+    return app
+
+@patch("src.message_bp.m")
+def test_show_messages_mock(mock_m, app_inst):
+    """Send a GET with good auth and expect a message."""
+    mock_m.Message.query.filter.return_value.all.return_value = [
+        m.Message(username=FAKER.word(), message_text=FAKER.text())
+            for number in range(10000)
+    ]
+    with app_inst.test_request_context("/app/messages"):
+        # You *must* use an application or request context when dealing with Flask.g !!!
+        g.user = MagicMock(username="not a user")
+        resp = get_messages()
+    assert json.loads(resp)
