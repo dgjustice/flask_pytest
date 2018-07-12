ca3f3ae0fbd2dfe9114b45463940eed948c19fbc
Daniel <djustice@wificidr.net>
2018-07-12 16:20:21 -0500

    Refactored to use Faker.

--- tests/load_db_data.py
+++ tests/load_db_data.py
@@ -1,43 +1,32 @@
 """Initialization script to load test data into the DB."""
+import random
 import src.models as m
 from src import DB
+from faker import Faker
+
+FAKER = Faker()
 
 def init_db(app):
     """Init the DB with some fake data."""
     with app.app_context():
         DB.create_all()
+        users = [
+            m.User(
+                username=FAKER.word(),
+                password=FAKER.password(),
+                nick="".join(FAKER.words())
+            ) for number in range(10)
+        ]
+        # We need at least one we can predict
+        users.append(m.User(username="foo", password="foo_pass", nick="foobar"))
         db_items = [
-            m.User(
-                username="foo",
-                password="foo_pass",
-                nick="foober"
-            ),
-            m.User(
-                username="bar",
-                password="bar_pass",
-                nick="barber"
-            ),
-            m.User(
-                username="qud",
-                password="qud_pass",
-                nick="qudber"
-            ),
-            m.Message(
-                username="foo",
-                message_text="Goofy message from foo!"
-            ),
-            m.Message(
-                username="foo",
-                message_text="Another message from foo!"
-            ),
-            m.Message(
-                username="bar",
-                message_text="Bar says hello!"
-            ),
-            m.Message(
-                username="qud",
-                message_text="Qud wants to go to the movies!"
-            ),
+            *users,
+            *[
+                m.Message(
+                    username=random.choice(users).username,
+                    message_text=FAKER.catch_phrase()
+                ) for number in range(10000)
+            ]
         ]
         for item in db_items:
             # pylint: disable=no-member
