6c9d16ce30e42b8481e72c0aeae12235a82e8581
Daniel <djustice@wificidr.net>
2018-07-13 21:36:35 -0500

    Modified patch to reflect new method.

--- tests/test_messages_unit.py
+++ tests/test_messages_unit.py
@@ -9,7 +9,7 @@ from unittest.mock import Mock, patch, MagicMock
 from flask import g
 from faker import Faker
 from src.app import create_app
-from src.message_bp import get_messages
+from src.message_bp import retrieve_message_text
 import src.models as m
 from tests.load_db_data import init_db
 
@@ -22,15 +22,15 @@ def app_inst():
     app.debug = True
     return app
 
-@patch("src.message_bp.m")
-def test_show_messages_mock(mock_m, app_inst):
+@patch("src.message_bp.query_message_by_user")
+def test_retrieve_message_text(mock_q, app_inst):
     """Send a GET with good auth and expect a message."""
-    mock_m.Message.query.filter.return_value.all.return_value = [
+    mock_q.return_value = [
         m.Message(username=FAKER.word(), message_text=FAKER.text())
             for number in range(10000)
     ]
     with app_inst.test_request_context("/app/messages"):
         # You *must* use an application or request context when dealing with Flask.g !!!
         g.user = MagicMock(username="not a user")
-        resp = get_messages()
+        resp = retrieve_message_text()
     assert json.loads(resp)
